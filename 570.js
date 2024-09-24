74855fe6bda1b3ea523bfc6683ddcfc51dc86452
Reapl.LLC <167105730+aiyaas@users.noreply.github.com>
2024-09-24 17:44:57 +0700

    KEYS

---
+++ 570.js
@@ -0,0 +1,7 @@
+/*
+ * Paste your_api_key_gemini-1.5-pro
+ * Get key https://aistudio.google.com/app/apikey
+ */
+module.exports = [
+     // Enter your api key
+]
--- msc_/api_key.js
+++
@@ -1,5 +0,0 @@
-/* Paste api_key_gemini-1.5-pro
- * get key https://aistudio.google.com/app/apikey
- *
- */
-module.exports = '' || false
