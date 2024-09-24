b667264546cb9bed7232189c9928da7e6a2a473c
Reapl.LLC <167105730+aiyaas@users.noreply.github.com>
2024-09-24 17:40:46 +0700

    SESSIONS

--- sdk_save.js
+++ sdk_save.js
@@ -1,8 +1,7 @@
 'use strict';
+
 module.exports = (userText, responseText) => {
-    const fs = require('fs');
-    fs.appendFileSync('mcs_sdk_msg.ini', `INPUT:  ${userText}\nAI_SDK:  ${responseText}\n\n`, (error) => {
+    require('fs').appendFileSync('./mcs_/mcs_sdk_msg.ini', `INPUT:  ${userText}\nAI_SDK:  ${responseText}\n\n`, (error) => {
         if (error) console.error(error.stack);
     });
 };
-
